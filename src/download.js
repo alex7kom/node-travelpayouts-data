7aa7fcb9296f1b5496977d620b42cdc119f71e10
Alexey Komarov <alex7kom@gmail.com>
2019-07-28 13:32:25 +0300

    retry on 404 when downloading data

--- src/download.js
+++ src/download.js
@@ -9,6 +9,11 @@ const downloadData = require('dl-data');
 const baseDir = path.join(__dirname, '..');
 const files = require('./files.json');
 
-downloadData(baseDir, files).then(() => {
+downloadData(baseDir, files, {
+  retry: {
+    retries: 10,
+    statusCodes: [404, 408, 413, 429, 500, 502, 503, 504]
+  }
+}).then(() => {
   console.log('All files downloaded!');
 });
